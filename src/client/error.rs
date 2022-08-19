bcf88c30cd3730939dcffd89a6ffbb1cc1fe4ae3
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-08-19 05:12:40 +0300

    error docs

--- src/client/error.rs
+++ src/client/error.rs
@@ -6,39 +6,45 @@ use crate::error::RpcResponseError;
 #[derive(Error, Debug)]
 pub enum Error {
 
+    /// Underlying WebSocket error
     #[error("WebSocket error: {0}")]
     WebSocketError(#[from] WebSocketError),
-
+    /// RPC call timeout
     #[error("RPC request timeout")]
     Timeout,
+    /// Unable to send shutdown message to receiver
     #[error("Receiver ctl failure")]
     ReceiverCtl,
+    /// Received message is smaller than the minimum header size
     #[error("Invalid header size")]
     HeaderSize,
-    // ~
+    /// RPC call succeeded but no data was received in success response
     #[error("RPC: no data in success response")]
     NoDataInSuccessResponse,
+    /// RPC call failed but no data was received in error response
     #[error("RPC: no data in error response")]
     NoDataInErrorResponse,
+    /// Unable to deserialize response data
     #[error("RPC: error deserializing response data")]
     ErrorDeserializingResponseData,
-    // Data(Vec<u8>),
+    /// Response produced an unknown status code
     #[error("RPC: status code {0}")]
     StatusCode(u32),
-    // RpcCall(u32,Option<Vec<u8>>),
+    /// RPC call executed successfully but produced an error response
     #[error("RPC: response error {0:?}")]
     RpcCall(RpcResponseError),
-    // RpcCall(u32,Option<&[u8]>),
+    /// Unable to serialize borsh data    
     #[error("RPC: borsh serialization error")]
     BorshSerialize,
+    /// Unable to deserialize borsh data
     #[error("RPC: borsh deserialization error")]
     BorshDeserialize,
-    
-    // borsh
+    /// RPC call succeeded, but error occurred deserializing borsh response
     #[error("RPC: borsh error deserializing response")]
     BorshResponseDeserialize,
 }
 
+/// Transform Error into JsValue containing the error message
 impl Into<JsValue> for Error {
     fn into(self) -> JsValue {
         JsValue::from(format!("{}",self).to_string())
